5704b8a4e79e3f7c2ebee343c70c879da6e8b532
Nicolas Aguttes <nicolas.aguttes@gmail..com>
2023-11-30 06:33:58 +0100

    adding comments

--- External_converters/RC204_4C.js
+++ External_converters/RC204_4C.js
@@ -13,9 +13,13 @@ const definition = {
     vendor: 'OWON', // Vendor of the device (only used for documentation and startup logging)
     description: 'OWON remote control RC204', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
     fromZigbee: [fz.battery,fz.command_toggle,fz.command_on_state,fz.command_off_state], // We will add this later
-    toZigbee: [], // Should be empty, unless device can be controlled (e.g. lights, switches).
-    exposes: [e.battery(), e.action(['toggle_un','toggle_deux','toggle_trois','toggle_quatre']),e.binary('state_un', exposes.access.STATE, true, false)], // Defines what this device exposes, used for e.g. Home Assistant discovery and in the frontend
+    toZigbee: [], 
+    // The on/off buttons set the states (state_un, state_2, ...) to ON or OFF, for this converter only 'state_un'
+    // is implemented in order to trap a state change in an automation
+    exposes: [e.battery(), e.action(['toggle_un','toggle_deux','toggle_trois','toggle_quatre']),e.binary('state_un', exposes.access.STATE, true, false)], 
     meta: {multiEndpoint: true},
+    // change the values 'un', 'deux", ... by the walues you want, it will return the action toggle_<your value>
+    // endpoint 1 matches the button with 1 dot, endpoint 2 the one with 2 dots and so on
     endpoint: (device) => {
         return {'un': 1, 'deux': 2, 'trois': 3, 'quatre': 4};
     }
